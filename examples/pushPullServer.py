805deb2948acefea110e878bbfbcd518bd3ce1f9
Lucas Lira Gomes <x8lucas8x@gmail.com>
2014-12-25 16:31:17 -0300

    Fixed Push-Pull server example.

--- examples/pushPullServer.py
+++ examples/pushPullServer.py
@@ -3,5 +3,5 @@ from zeroless import bind
 # The pull server binds to port 12345 and waits for incoming messages.
 sock = bind(port=12345)
 
-for msg in sock.listen_for_pull():
+for msg in sock.listen_for_push():
     print(msg)
