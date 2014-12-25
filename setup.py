87b995afa994843aa22c9d95597bd722a109849b
Lucas Lira Gomes <x8lucas8x@gmail.com>
2014-12-25 16:42:42 -0300

    Updated version in the setup.py file.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='zeroless',
-      version='0.1.1',
+      version='0.2.1',
       description='A pythonic approach for distributed systems with ZeroMQ.',
       long_description=readme(),
       classifiers=[
