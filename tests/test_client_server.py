95b270b86e88819969f34e27c68d58120d6fe65a
Lucas Lira Gomes <x8lucas8x@gmail.com>
2015-05-01 17:25:02 -0300

    Added a test to the addresses property in the Client class.

--- tests/test_client_server.py
+++ tests/test_client_server.py
@@ -1,6 +1,6 @@
 import pytest
 
-from zeroless import (Server)
+from zeroless import (Server, Client)
 
 class TestClientServer:
     def test_server_port_property(self):
@@ -8,3 +8,12 @@ class TestClientServer:
         server = Server(port=port)
 
         assert server.port == port
+
+    def test_client_addresses_property(self):
+        client = Client()
+        addresses = (('10.0.0.1', 1567), ('10.0.0.2', 1568), ('10.0.0.3', 1569))
+
+        for ip, port in addresses:
+            client.connect(ip, port)
+
+        assert client.addresses == addresses
